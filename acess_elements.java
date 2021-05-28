2e6e7704e075e1195b484e34d6cde3df7b4d8180
Arnab Dey <arnabdey132@hotmail.com>
2021-05-28 08:49:06 +0530

    Create acess_elements.java

---
+++ acess_elements.java
@@ -0,0 +1,22 @@
+// Java code to access elements from a PriorityQueue
+
+import java.util.*;
+
+class PriorityQueueDemo {
+
+	// Main Method
+	public static void main(String[] args)
+	{
+
+		// Creating a priority queue
+		PriorityQueue<String> pq = new PriorityQueue<>();
+		pq.add("Geeks");
+		pq.add("For");
+		pq.add("Geeks");
+		System.out.println("PriorityQueue: " + pq);
+
+		// Using the peek() method
+		String element = pq.peek();
+		System.out.println("Accessed Element: " + element);
+	}
+}
